de3fdcd9f158c3639704e7534d6e7994fecd63d6
RooRsmoka <chri.disa96@gmail.com>
2022-01-11 16:59:30 +0100

    creazione array di oggetti (Milestone 1)

---
+++ main.js
@@ -0,0 +1,50 @@
+// MILESTONE 1
+// Creo un array di oggetti che rappresentano i post, ogni oggetto avrà le informazioni 
+// necessarie per la stampa del relativo post.
+const postList = [
+    {
+        authorName: 'Phil mangione',
+        authorImg: 'Profile-Pic-1.jpg',
+        postImg: 'Post-Image',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postId: 1,
+        date: 05-19-2021,
+        likes: 183,
+    },
+    {
+        authorName: 'John Wayne',
+        authorImg: 'Profile-Pic-2.jpg',
+        postImg: 'Post-Image',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postId: 2,
+        date: 11-05-2017,
+        likes: 49,
+    },
+    {
+        authorName: 'Mark Philips',
+        authorImg: 'Profile-Pic-3.jpg',
+        postImg: 'Post-Image',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postId: 3,
+        date: 06-22-2018,
+        likes: 68,
+    },
+    {
+        authorName: 'Sarah Connor',
+        authorImg: 'Profile-Pic-4.jpg',
+        postImg: 'Post-Image',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postId: 4,
+        date: 01-01-2019,
+        likes: 132,
+    },
+    {
+        authorName: 'Steve Jobless',
+        authorImg: 'Profile-Pic-5.jpg',
+        postImg: 'Post-Image',
+        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postId: 5,
+        date: 08-15-2020,
+        likes: 530,
+    },
+];
